17b91626825e4bf441da1b9d32ac26f61bcb7312
ahmedkamal6 <87461881+ahmedkamal6@users.noreply.github.com>
2023-04-06 04:34:06 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,8 @@
+const APP_ID = "f83Is2y7L425rxl8";
+const APP_SECRET = "4Yn8RkxDXN71Q3p0";
+const sha1 = require("js-sha1");
+const axios = require("axios");
+
 const testPrice = () => {
   let config = {
     method: "get",
@@ -48,3 +53,43 @@ const testWebHook = () => {
       console.log(error);
     });
 };
+
+const testCallback = () => {
+    const config = {
+      method: "post",
+      url: "http://localhost:5000/alchemyOnRamp/callback",
+      data: {
+        appId: APP_ID,
+        orderNo: "ORDER123",
+        email: "test@test.com",
+        crypto: "BTC",
+        cryptoPrice: "60000",
+        cryptoQuantity: "0.01",
+        payType: "credit_card",
+        fiat: "USD",
+        amount: "100",
+        payTime: "2022-04-04 14:30:00",
+        network: "BTC",
+        address: "123abc",
+        txTime: "2022-04-04 14:45:00",
+        txHash: "0x123abc",
+        status: "PAY_SUCCESS",
+        message: "Payment successful",
+        merchantOrderNo: "MERCHANT_ORDER123",
+        networkFee: "0.0005",
+        rampFee: "0.02",
+        signature: sha1(APP_ID + APP_SECRET + String("ORDER123" + "BTC" + "BTC" + "123abc")),
+      },
+    };
+  
+    axios(config)
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.log("error");
+      });
+  };
+
+ 
+  module.exports = {testCallback,testPrice,testWebHook}
