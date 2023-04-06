37cd2e30332c7d3639a0c1adc14cf01163ba3f0e
ahmedkamal6 <87461881+ahmedkamal6@users.noreply.github.com>
2023-04-06 04:33:36 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const sha1 = require("js-sha1");
 const axios = require("axios");
+const {testPrice,testWebHook,testCallback} = require('./test') 
+
 const app = express();
 app.use(express.json());
 const APP_ID = "f83Is2y7L425rxl8";
 const APP_SECRET = "4Yn8RkxDXN71Q3p0";
 
-app.post("/webhook", (req, res) => {
-  // Verify the signature for authentication
+
+
+
+function verifySignature(req, res, next) {
   const SIGNATURE_VALIDITY_PERIOD = 5 * 60 * 1000;
   const receivedTimestamp = Date.now();
   const appId = req.get("appId");
@@ -27,6 +31,13 @@ app.post("/webhook", (req, res) => {
       returnMsg: "Invalid signature",
     });
   } else {
+    next();
+  }
+}
+
+//this webhook to listen for Alchemy Pay notification that the user has paid and we need to transfer the tokens
+app.post("/webhook",verifySignature, (req, res) => {
+  // Verify the signature for authentication
     const {
       orderNo,
       crypto,
@@ -55,7 +66,7 @@ app.post("/webhook", (req, res) => {
           headers: {
             appId: APP_ID,
             timestamp,
-            sign: sha1(appId + appSecret + String(timestamp)),
+            sign: sha1(APP_ID + APP_SECRET + String(timestamp)),
           },
         })
         .then((response) => {
@@ -74,28 +85,10 @@ app.post("/webhook", (req, res) => {
       returnMsg: "Notification sent to alchemy pay",
     });
   }
-});
-app.get("/price", (req, res) => {
+);
+//this endpoint sends the price of the currency to Alchemy Pay 
+app.get("/price",verifySignature, (req, res) => {
   // Verify the signature for authentication
-  const SIGNATURE_VALIDITY_PERIOD = 5 * 60 * 1000;
-  const receivedTimestamp = Date.now();
-  const appId = req.get("appId");
-  const appSecret = APP_SECRET;
-  const requestTimestamp = parseInt(req.get("timestamp"), 10);
-  const signatureAge = receivedTimestamp - requestTimestamp;
-  const signature = sha1(appId + appSecret + String(requestTimestamp));
-
-  if (
-    signature !== req.get("sign") ||
-    signatureAge > SIGNATURE_VALIDITY_PERIOD
-  ) {
-    console.log("Invalid signature");
-    return res.status(400).json({
-      success: false,
-      returnCode: "9999",
-      returnMsg: "Invalid signature",
-    });
-  } else {
     const { crypto } = req.body;
     res.status(200).json({
       success: true,
@@ -116,7 +109,8 @@ app.get("/price", (req, res) => {
       },
     });
   }
-});
+);
+//this is the end point that Alchemy Pay will use when transaction complete and send all the information which then will be added to our db
 app.post("/alchemyOnRamp/callback", (req, res) => {
 
   const { appId, signature, ...transactionData } = req.body;
@@ -143,44 +137,11 @@ app.post("/alchemyOnRamp/callback", (req, res) => {
   console.log(transactionData)
   //alchemyTransactions.createTransaction(transactionData);
 });
+
 app.listen(5000, () => {
   console.log("Webhook listening on port 3000");
 });
 
-const testCallback = () => {
-  const config = {
-    method: "post",
-    url: "http://localhost:5000/alchemyOnRamp/callback",
-    data: {
-      appId: APP_ID,
-      orderNo: "ORDER123",
-      email: "test@test.com",
-      crypto: "BTC",
-      cryptoPrice: "60000",
-      cryptoQuantity: "0.01",
-      payType: "credit_card",
-      fiat: "USD",
-      amount: "100",
-      payTime: "2022-04-04 14:30:00",
-      network: "BTC",
-      address: "123abc",
-      txTime: "2022-04-04 14:45:00",
-      txHash: "0x123abc",
-      status: "PAY_SUCCESS",
-      message: "Payment successful",
-      merchantOrderNo: "MERCHANT_ORDER123",
-      networkFee: "0.0005",
-      rampFee: "0.02",
-      signature: sha1(APP_ID + APP_SECRET + String("ORDER123" + "BTC" + "BTC" + "123abc")),
-    },
-  };
-
-  axios(config)
-    .then(function (response) {
-      console.log(response.data);
-    })
-    .catch(function (error) {
-      console.log("error");
-    });
-};
-testCallback()
+//testPrice()
+testWebHook()
+//testCallback()
